9200012a4cac38722ad4d023c47d6069bc8ec269
Maciej <sppmacd@pm.me>
2022-06-08 20:15:57 +0200

    SQL: Implement parsing of multiple columns

--- db/sql/Parser.cpp
+++ db/sql/Parser.cpp
@@ -4,32 +4,36 @@
 namespace Db::Sql {
 
 Core::DbErrorOr<Core::AST::Select> Parser::parse_select() {
-    auto select = m_tokens[m_offset];
+    auto select = m_tokens[m_offset++];
     if (select.type != Token::Type::KeywordSelect)
         return Core::DbError { "Expected 'SELECT'" };
-    m_offset++;
 
-    Core::AST::SelectColumns columns;
-    auto column = m_tokens[m_offset];
-    if (column.type == Token::Type::Asterisk)
-        columns = {};
-    else if (column.type == Token::Type::Identifier)
-        columns = { { column.value } };
-    else
-        return Core::DbError { "Expected column name" };
-    m_offset++;
+    std::set<std::string> columns;
+    while (true) {
+        auto column = m_tokens[m_offset++];
+        if (column.type == Token::Type::Asterisk)
+            break;
 
-    auto from = m_tokens[m_offset];
+        if (column.type == Token::Type::Identifier)
+            columns.insert(column.value);
+        else
+            return Core::DbError { "Expected column name" };
+
+        auto comma = m_tokens[m_offset];
+        if (comma.type != Token::Type::Comma)
+            break;
+        m_offset++;
+    }
+
+    auto from = m_tokens[m_offset++];
     if (from.type != Token::Type::KeywordFrom)
-        return Core::DbError { "Expected 'FROM'" };
-    m_offset++;
+        return Core::DbError { "Expected 'FROM', got " + from.value };
 
-    auto from_token = m_tokens[m_offset];
+    auto from_token = m_tokens[m_offset++];
     if (from_token.type != Token::Type::Identifier)
         return Core::DbError { "Expected table name after 'FROM'" };
-    m_offset++;
 
-    return Core::AST::Select { columns, from_token.value, {}, {}, {} };
+    return Core::AST::Select { { columns }, from_token.value, {}, {}, {} };
 }
 
 }
