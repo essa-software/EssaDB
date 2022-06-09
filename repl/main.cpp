fb3adfb6f1e8c4fab0fa4736f32cfe65e484d95d
Maciej <sppmacd@pm.me>
2022-06-09 11:42:25 +0200

    Repl: Start working on SQL REPL :^)

--- repl/main.cpp
+++ repl/main.cpp
@@ -9,11 +9,17 @@
 int main() {
     Db::Core::Database db;
     db.create_table("test");
-    auto result = Db::Sql::run_query(db, "SELECT * FROM test");
-    if (result.is_error()) {
-        std::cout << "Error: " << result.release_error().message() << std::endl;
-        return 1;
+    while (true) {
+        std::cout << "> ";
+        std::string query;
+        if (!std::getline(std::cin, query))
+            return 1;
+
+        auto result = Db::Sql::run_query(db, query);
+        if (result.is_error())
+            std::cout << "Error: " << result.release_error().message() << std::endl;
+        else
+            result.release_value().repl_dump(std::cout);
     }
-    result.release_value().repl_dump(std::cout);
     return 0;
 }
