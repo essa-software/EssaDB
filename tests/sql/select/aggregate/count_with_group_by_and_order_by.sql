8002566306c127b0eb635d95dff610db7f94fe22
Maciej <sppmacd@pm.me>
2022-06-19 17:24:14 +0200

    Core: Unskip ORDER BY with GROUP BY testcase
    Also fix it since it was invalid (we don't support ordering by aliases)

--- tests/sql/select/aggregate/count_with_group_by_and_order_by.sql
+++ tests/sql/select/aggregate/count_with_group_by_and_order_by.sql
@@ -1,4 +1,9 @@
 IMPORT CSV 'aggregate.csv' INTO test;
 
--- skip: segfaults
-SELECT [group], COUNT(id) AS [COUNTED], LEN([group]) AS [len] FROM test GROUP BY [group] ORDER BY COUNTED DESC;
+-- Ordering by grouped by column
+-- output:
+-- | group | COUNTED | len |
+-- |     C |       3 |   1 |
+-- |     B |       2 |   1 |
+-- |    AA |       2 |   2 |
+SELECT [group], COUNT(id) AS [COUNTED], LEN([group]) AS [len] FROM test GROUP BY [group] ORDER BY [group] DESC;
