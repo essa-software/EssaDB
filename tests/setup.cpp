20e2e429179b311fe846553a96f60cf12368b67f
Maciej <sppmacd@pm.me>
2022-06-19 10:01:06 +0200

    Tests: Return 1 if test fails

--- tests/setup.cpp
+++ tests/setup.cpp
@@ -20,15 +20,18 @@ extern std::map<std::string, TestFunc> get_tests();
 int main() {
     std::map<std::string, TestFunc> funcs = get_tests();
 
+    bool failed = false;
+
     for (auto const& func : funcs) {
         auto f = func.second();
         if (f.is_error()) {
             std::cout << "\e[31;1mFAIL\e[m " << func.first << " " << f.release_error().message() << std::endl;
+            failed = true;
         }
         else {
             std::cout << "\e[32;1mPASS\e[m " << func.first << std::endl;
         }
     }
 
-    return 0;
+    return failed ? 1 : 0;
 }
