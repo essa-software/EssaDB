be625779e6d078c1e25ebd7d0ae1d759427a7749
Maciej <sppmacd@pm.me>
2022-06-19 15:22:47 +0200

    Tests: Add tests for 'expected' parse errors

---
+++ tests/sql/parser/expected_errors.sql
@@ -0,0 +1,44 @@
+-- error: Expected expression, got 'EOF'
+SELECT
+
+-- error: Expected expression, got 'FROM'
+SELECT FROM test;
+
+-- error: Expected expression, got '('
+SELECT (
+
+-- error: Expected expression, got ')'
+SELECT )
+
+-- error: Expected expression, got '('
+SELECT ()
+
+-- error: Expected identifier in aggregate function, got 'EOF'
+SELECT COUNT(
+
+-- error: Expected expression, got 'EOF'
+SELECT CHAR(
+
+-- error: Expected ')' to close function, got 'EOF'
+SELECT CHAR(a
+
+-- error: Expected expression, got ','
+SELECT CHAR(,
+
+-- error: Expected expression, got 'EOF'
+SELECT CHAR(a,
+
+-- error: Expected expression, got ','
+SELECT CHAR(a,,,,,a)
+
+-- error: Expected ')' to close function, got 'FROM'
+SELECT CHAR(a FROM test
+
+-- error: Expected expression, got 'FROM'
+SELECT CHAR(a, FROM test;
+
+-- error: Expected table name after 'FROM', got 'EOF'
+SELECT COUNT(a) FROM
+
+-- error: Expected identifier in aggregate function, got ')'
+SELECT COUNT()
