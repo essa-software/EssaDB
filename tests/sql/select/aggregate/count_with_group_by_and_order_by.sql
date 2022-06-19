8cc61b3a4c39dccb6bcd2456a7fb914059285154
Maciej <sppmacd@pm.me>
2022-06-19 12:57:40 +0200

    Tests: Remove display from one test

--- tests/sql/select/aggregate/count_with_group_by_and_order_by.sql
+++ tests/sql/select/aggregate/count_with_group_by_and_order_by.sql
@@ -1,7 +1,4 @@
 IMPORT CSV 'aggregate.csv' INTO test;
 
--- display
-SELECT * FROM test;
-
 -- skip: segfaults
 SELECT [group], COUNT(id) AS [COUNTED], LEN([group]) AS [len] FROM test GROUP BY [group] ORDER BY COUNTED DESC;
