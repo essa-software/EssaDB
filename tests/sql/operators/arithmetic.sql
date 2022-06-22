40006604382ccaeb60d30cef1a7daa3fe113b424
Maciej <sppmacd@pm.me>
2022-06-22 21:17:26 +0200

    Tests: Add tests for division by 0 and operations with null

--- tests/sql/operators/arithmetic.sql
+++ tests/sql/operators/arithmetic.sql
@@ -4,17 +4,47 @@
 -- |                       74 |
 SELECT 69 + 5;
 
+-- output:
+-- | ArithmeticOperator(69,null) |
+-- |                        null |
+SELECT 69 + NULL;
+
+-- output:
+-- | ArithmeticOperator(null,5) |
+-- |                       null |
+SELECT NULL + 5;
+
 -- subtraction
 -- output:
 -- | ArithmeticOperator(69,5) |
 -- |                       64 |
 SELECT 69 - 5;
 
+-- output:
+-- | ArithmeticOperator(69,null) |
+-- |                        null |
+SELECT 69 - NULL;
+
+-- output:
+-- | ArithmeticOperator(null,5) |
+-- |                       null |
+SELECT NULL - 5;
+
 -- multiplication
 -- output:
 -- | ArithmeticOperator(69,5) |
 -- |                      345 |
 SELECT 69 * 5;
+
+-- output:
+-- | ArithmeticOperator(69,null) |
+-- |                        null |
+SELECT 69 * NULL;
+
+-- output:
+-- | ArithmeticOperator(null,5) |
+-- |                       null |
+SELECT NULL * 5;
 
 -- output:
 -- | ArithmeticOperator(ArithmeticOperator(33,36),5) |
@@ -31,3 +61,17 @@ SELECT 5 * (33 + 36);
 -- | ArithmeticOperator(69,5) |
 -- |                       13 |
 SELECT 69 / 5;
+
+-- output:
+-- | ArithmeticOperator(69,null) |
+-- |                        null |
+SELECT 69 / NULL;
+
+-- output:
+-- | ArithmeticOperator(null,5) |
+-- |                       null |
+SELECT NULL / 5;
+
+-- division
+-- error: Cannot divide by 0
+SELECT 69 / 0;
