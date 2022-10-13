0c020ddc033b5508e6d19fd88f902e178e51d6cf
Maciej <sppmacd@pm.me>
2022-10-13 11:59:18 +0200

    Core: Add YEAR, MONTH, DAY functions
    They return year, month and day from timestamp using local timezone.

---
+++ tests/sql/functions/date/day_month_year.sql
@@ -0,0 +1,14 @@
+CREATE TABLE events (date_ TIME);
+
+INSERT INTO events (date_) VALUES(#1970-01-19#);
+INSERT INTO events (date_) VALUES(#2010-11-17#);
+INSERT INTO events (date_) VALUES(#2038-11-01#);
+INSERT INTO events (date_) VALUES(#2100-02-26#);
+
+-- display
+-- | Year | Month | Day |
+-- | 1970 |     1 |  19 |
+-- | 2010 |    11 |  17 |
+-- | 2038 |    11 |   1 |
+-- | 2100 |     2 |  26 |
+SELECT YEAR(date_) AS Year, MONTH(date_) AS Month, DAY(date_) AS Day FROM events;
