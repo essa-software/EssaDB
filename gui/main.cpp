97dfab978ade8286cf83bf6811efc0d58d83d8bf
Adam Ratajczak <a3.ratajczak@gmail.com>
2022-10-29 18:30:16 +0200

    essadb-gui: close MySQL connection

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,7 +1,6 @@
 #include "DatabaseModel.hpp"
 #include "ImportCSVDialog.hpp"
 #include "gui/ConnectToMySQLDialog.hpp"
-#include "gui/ConnectToMySQLDialog.hpp"
 #include <EssaGUI/eml/EMLResource.hpp>
 #include <EssaGUI/gui/Application.hpp>
 #include <EssaGUI/gui/Console.hpp>
@@ -12,8 +11,12 @@
 
 #include <mysql.h>
 
+int mode = 0;
+
 int main() {
     GUI::Application app;
+    MYSQL* connection;
+
     auto& window = app.create_host_window({ 1200, 700 }, "EssaDB");
 
     auto& container = window.set_main_widget<GUI::Container>();
@@ -23,6 +26,7 @@ int main() {
     auto run_button = container.find_widget_of_type_by_id_recursively<GUI::TextButton>("run_sql");
     auto import_button = container.find_widget_of_type_by_id_recursively<GUI::TextButton>("import_csv");
     auto connect_button = container.find_widget_of_type_by_id_recursively<GUI::TextButton>("connect_mysql");
+    auto close_connection = container.find_widget_of_type_by_id_recursively<GUI::TextButton>("close_connection");
     auto outline_container = container.find_widget_of_type_by_id_recursively<GUI::Container>("outline_container");
     outline_container->set_background_color(app.theme().sidebar);
 
@@ -36,16 +40,20 @@ int main() {
     auto& db_model = tree_view->create_and_set_model<EssaDB::DatabaseModel>(db);
 
     auto run_sql = [&](Util::UString const& query) {
-        console->append_content({ .color = Util::Color { 100, 100, 255 }, .text = "> " + query });
-        auto result = Db::Sql::run_query(db, query.encode());
-        if (result.is_error()) {
-            console->append_content({ .color = Util::Colors::Red, .text = Util::UString { result.release_error().message() } });
+        if (mode == 0) {
+            console->append_content({ .color = Util::Color { 100, 100, 255 }, .text = "> " + query });
+            auto result = Db::Sql::run_query(db, query.encode());
+            if (result.is_error()) {
+                console->append_content({ .color = Util::Colors::Red, .text = Util::UString { result.release_error().message() } });
+            }
+            else {
+                std::ostringstream oss;
+                result.release_value().repl_dump(oss);
+                console->append_content({ .color = Util::Colors::White, .text = Util::UString { oss.str() } });
+                db_model.update();
+            }
         }
-        else {
-            std::ostringstream oss;
-            result.release_value().repl_dump(oss);
-            console->append_content({ .color = Util::Colors::White, .text = Util::UString { oss.str() } });
-            db_model.update();
+        else if (mode == 1) {
         }
     };
 
@@ -73,11 +81,17 @@ int main() {
     };
 
     connect_button->on_click = [&]() {
+        if (mode == 1) {
+            console->append_content({
+                .color = Util::Colors::Red,
+                .text = Util::UString {
+                    fmt::format("You need to close the active conection in order to perform this operation!") },
+            });
+            return;
+        }
         auto& connect_mysql_dialog = window.open_overlay<EssaDB::ConnectToMySQLDialog>();
 
-        connect_mysql_dialog.on_ok = [&console, &connect_mysql_dialog](){
-            MYSQL *connection;
-
+        connect_mysql_dialog.on_ok = [&connection, &console, &connect_mysql_dialog]() {
             connection = mysql_init(NULL);
             if (connection == NULL) {
                 console->append_content({
@@ -88,20 +102,20 @@ int main() {
                 return false;
             }
 
-            if (!mysql_real_connect(connection, 
-                                    connect_mysql_dialog.address().data(), 
-                                    connect_mysql_dialog.username().data(), 
-                                    connect_mysql_dialog.password().data(), 
-                                    connect_mysql_dialog.database().data(),
-                                    std::stoi(connect_mysql_dialog.port()), NULL, 0)) {
+            if (!mysql_real_connect(connection,
+                    connect_mysql_dialog.address().data(),
+                    connect_mysql_dialog.username().data(),
+                    connect_mysql_dialog.password().data(),
+                    connect_mysql_dialog.database().data(),
+                    std::stoi(connect_mysql_dialog.port()), NULL, 0)) {
 
                 console->append_content({
                     .color = Util::Colors::Red,
                     .text = Util::UString {
                         fmt::format("Failed to connect to server: {}", mysql_error(connection)) },
                 });
-            return false;
-        }
+                return false;
+            }
 
             console->append_content({
                 .color = Util::Colors::Lime,
@@ -109,13 +123,40 @@ int main() {
                     fmt::format("Successfully connected to the database '{}'!", connect_mysql_dialog.database()) },
             });
 
+            mode = 1;
+
             return true;
         };
-        
+
         connect_mysql_dialog.run();
     };
+
+    close_connection->on_click = [&]() {
+        if (mode == 0) {
+            console->append_content({
+                .color = Util::Colors::Red,
+                .text = Util::UString {
+                    fmt::format("Cannot perform this operation on a closed connection!") },
+            });
+        }
+        else if (mode == 1) {
+            mode = 0;
+            mysql_close(connection);
+            console->append_content({
+                .color = Util::Colors::Lime,
+                .text = Util::UString {
+                    fmt::format("MySQL connection closed successfully!") },
+            });
+        }
+    };
+
     text_editor->on_enter = [&](Util::UString const& query) { run_sql(query); };
 
     app.run();
+
+    if (mode == 1) {
+        mysql_close(connection);
+    }
+
     return 0;
 }
