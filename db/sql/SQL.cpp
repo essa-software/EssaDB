691361772ac06a4e5fcd5672cb271797abe40304
Maciej <sppmacd@pm.me>
2022-11-17 22:37:32 +0100

    SQL: Use Util::display_error()

--- db/sql/SQL.cpp
+++ db/sql/SQL.cpp
@@ -3,6 +3,8 @@
 #include "Lexer.hpp"
 #include "Parser.hpp"
 
+#include <EssaUtil/DisplayError.hpp>
+#include <EssaUtil/Stream/MemoryStream.hpp>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
@@ -25,31 +27,13 @@ Core::DbErrorOr<Core::ValueOrResultSet> run_query(Core::Database& db, std::strin
 }
 
 void display_error(Db::Core::DbError const& error, ssize_t error_start, std::string const& query) {
-    std::cout << "\e[1;31mError: \e[m" << error.message() << std::endl;
-
-    std::istringstream iss { query };
-    ssize_t line_number = 1;
-    ssize_t last_index = 0;
-    while (true) {
-        std::string str;
-        if (!std::getline(iss, str))
-            break;
-        std::cout << std::setw(4) << line_number << " | ";
-        for (ssize_t s = 0; static_cast<size_t>(s) < str.size(); s++) {
-            if (s + last_index == error_start)
-                std::cout << "\e[31m" << str[s] << "\e[m";
-            else
-                std::cout << str[s];
-        }
-        std::cout << std::endl;
-        if (last_index <= error_start && last_index + str.size() >= static_cast<size_t>(error_start)) {
-            std::cout << "       ";
-            for (ssize_t s = 0; s < error_start - last_index; s++)
-                std::cout << " ";
-            std::cout << "^ \e[31m" << error.message() << "\e[m" << std::endl;
-        }
-        last_index = iss.tellg();
-    }
+    Util::ReadableMemoryStream stream { { reinterpret_cast<uint8_t const*>(query.c_str()), query.size() } };
+    Util::display_error(stream,
+        Util::DisplayedError {
+            .message = Util::UString { error.message() },
+            .start_offset = static_cast<size_t>(error_start),
+            .end_offset = static_cast<size_t>(error_start + 1),
+        });
 }
 
 }
