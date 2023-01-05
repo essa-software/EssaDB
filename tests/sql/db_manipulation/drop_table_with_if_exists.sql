b6202445739fe96f6ac3340140a8eeecc596c4b1
Maciej <sppmacd@pm.me>
2023-01-06 00:02:18 +0100

    Tests: Add more tests for CREATE TABLE IF NOT EXISTS
    This now actually check if this is allowed to run CREATE TABLE IF NOT
    EXISTS if a table exists :^)

--- tests/sql/db_manipulation/drop_table_with_if_exists.sql
+++ tests/sql/db_manipulation/drop_table_with_if_exists.sql
@@ -1,5 +1,14 @@
 CREATE TABLE IF NOT EXISTS test;
 
+-- error: Table 'test' already exists
+CREATE TABLE test;
+
+-- output:
+-- null
+CREATE TABLE IF NOT EXISTS test;
+
+-- output:
+-- null
 DROP TABLE IF EXISTS test;
 
 -- error: Nonexistent table: test
