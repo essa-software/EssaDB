e31df7164a9cc1b868b9c2a3d10d2cbab8abf6b7
Maciej <sppmacd@pm.me>
2023-01-16 13:45:56 +0100

    Tests: Fix is null check expression
    This was invalid from the beginning, now uncovered by trailing code
    check.

--- tests/sql/db_manipulation/multiple_checks_alter.sql
+++ tests/sql/db_manipulation/multiple_checks_alter.sql
@@ -1,5 +1,5 @@
 CREATE TABLE test (id INT, value INT DEFAULT 3000 CHECK value > 1000 CONSTRAINT id_check CHECK id != null CONSTRAINT value_check CHECK value != 3000);
-ALTER TABLE test ALTER CHECK value != 3000, ALTER CONSTRAINT id_check CHECK value > 1000, ALTER CONSTRAINT value_check CHECK id NOT NULL;
+ALTER TABLE test ALTER CHECK value != 3000, ALTER CONSTRAINT id_check CHECK value > 1000, ALTER CONSTRAINT value_check CHECK id != NULL;
 -- error: Values doesn't match 'id_check' check rule specified for this table
 INSERT INTO test (id, value) VALUES (1, 1000);
 -- error: Values doesn't match general check rule specified for this table
