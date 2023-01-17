69d4481b0ea46a867e24792b0302e56f02dc5452
Maciej <sppmacd@pm.me>
2023-01-17 09:49:35 +0100

    Tests: Add tests for case insensitivity

---
+++ tests/sql/parser/case_insensitivity.sql
@@ -0,0 +1,50 @@
+-- Keywords
+
+-- output:
+-- | 1 |
+-- | 1 |
+select 1;
+
+-- output:
+-- | 1 |
+-- | 1 |
+SelEct 1;
+
+-- output:
+-- | 1 |
+-- | 1 |
+SelEct 1;
+
+-- True/False (handled specially)
+
+-- output:
+-- | true |
+-- | true |
+SELECT TRUE;
+
+-- output:
+-- | true |
+-- | true |
+SELECT true;
+
+-- output:
+-- | true |
+-- | true |
+SELECT TrUe;
+
+-- output:
+-- | false |
+-- | false |
+SELECT FALSE;
+
+-- output:
+-- | false |
+-- | false |
+SELECT false;
+
+-- output:
+-- | false |
+-- | false |
+SELECT FaLsE;
+
+-- TODO: Asc/Desc
