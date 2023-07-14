e3274da67d1363d37b856fb37018c92637372bce
Maciej <sppmacd@pm.me>
2023-07-14 18:59:44 +0200

    Build: Update Essa to 12a1dea
    30 commit(s):
    - Util: Make ESSA_UNREACHABLE/ESSA_TODO well defined
    - Util/GenericParser: Add some helpers for ranges
    - Util: Support displaying file name in DisplayError
    - Util: Add TemporaryChange
    - Util: Explicitly qualify fmt::format_to
    - Add conditional operators foe Ustrings
    - Util: Support overridding token type to string
    - Util/DisplayError: Output to stderr
    - Build: Add a way to compile only EssaUtil
    - Util: Move TODO to Util namespace
    - Util: Expose Coordinates type
    - GUI: Draw border around ScrollableContainer
    - GUI: Relativize events passed to ScrollableContainer widget
    - GUI: Don't handle events twice in ScrollableContainer
    - GUI: Add Container::remove_widget(Widget&)
    - GUI: Fix compilation of Container::find()
    - GUI: Add EventLoop::deferred_invoke()
    - GUI: Implement EML loader for ScrollableContainer
    - GUI: Add Container::find<T>(id)
    - GUI: Add Container::remove_widget(index)
    - GUI/Widgets: Take cross size value into account in BoxLayout
    - Util: Add UString::format()
    - Util: Properly handle rects with negative sizes in instersection()
    - Util: Add Rect::with_negative_size_fixed
    - GUI: ClipViewScope WTR draw call in ContextMenu
    - GUI: Fix null deref in ContextMenu event handler
    - GUI: Support RichText in Textfields
    - Util: Add MultidimensionalArray::try_for_each_cell()
    - Util: Fix index_to_coords() after new Vectors update
    - Util: Support specifying base for UString parse

--- db/sql/SQL.cpp
+++ db/sql/SQL.cpp
@@ -30,6 +30,7 @@ void display_error(SQLError const& error, ssize_t error_start, ssize_t error_end
     Util::ReadableMemoryStream stream { { reinterpret_cast<uint8_t const*>(query.c_str()), query.size() } };
     Util::display_error(stream,
         Util::DisplayedError {
+            .file_name = "<query>",
             .message = Util::UString { error.message() },
             .start_offset = static_cast<size_t>(error_start),
             .end_offset = static_cast<size_t>(error_end),
